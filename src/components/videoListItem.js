ffde90e4447f7e755ca197695b6b6b221cfb16ab
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-05 17:07:09 +0100

    We have created the VideoDetails components. Please note that it is tateless because we urrently get all the attributes needed from the fetch made by the App component, so then title, description and videoId can be retrieved from the video passed in as props

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const VideoListItem = ({video}) => {
-    console.log(video);
+    // console.log(video);
     return (
         <li className='list-group-item'>
             <div className='video-list media'>
