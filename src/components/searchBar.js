bc1af535c885f696e61e6bc8df831282e0a8aee8
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-05 15:11:31 +0100

    We are defining a state as an object.

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {term: ''};
+    }
+
     render() {
         return (
             <input
@@ -9,9 +15,7 @@ class SearchBar extends Component {
         );
     }
 
-    onInputChange(e) {
-        console.log('change', e.target.value);
-    }
+    onInputChange = (e) => console.log('change', e.target.value);
 }
 
 export default SearchBar;
