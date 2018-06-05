53fbb75cd5a83ea68a0601c9b9e507d49177310d
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-05 17:27:00 +0100

    We create a new pair key value in the App's state called: selectedVideo: video, with the aim to let the user pick the one he wants to see. In addition we need to pass as a prop the selectedVideo to the videoDetails to render it!

--- src/index.js
+++ src/index.js
@@ -12,11 +12,17 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'goal'}, (videos) => {
             this.setState(() => {
-                return {videos};
+                return {
+                    videos: videos,
+                    selectedVideo: videos[0]
+                };
             })
         });
     }
@@ -25,7 +31,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
-                <VideoDetail video={this.state.videos[0]}/>
+                <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList videos={this.state.videos}/>
             </div>
         );
