8c4f15f703118f13cf364e5935a17a7460d2feb3
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-05 16:03:49 +0100

    We have the initial search into App's state

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyATS38RuWTxH2vL9QSu_NTjE5JXlU3MzgU';
 
-YTSearch({key: API_KEY, term: 'surf'}, function (data) {
-    console.log(data);
-});
 
 class App extends Component {
     constructor(props) {
         super(props);
+
         this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surf'}, (data) => {
+            this.setState(() => {
+                return {videos: data};
+            })
+        });
     }
 
     render() {
