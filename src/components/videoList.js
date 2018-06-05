598685796b1865dc53703ce185fbbfa1cea7e97f
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-05 16:22:38 +0100

    We show a list of the thumbnails taaken frommthe App feth to youtube

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -3,7 +3,9 @@ import React from 'react';
 const VideoList = (props) => {
     return (
         <ul className='col-md-4 list-group'>
-            {props.videos.length}
+            {props.videos && props.videos.map((video) => {
+                return <img src={video.snippet.thumbnails.default.url}/>
+            })}
         </ul>
     );
 };
