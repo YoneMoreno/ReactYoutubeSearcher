723da4abbd4e772006a1f067b99cf73db1480433
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-05 17:17:52 +0100

    Fix error: VideoDetails expected to get instantly the video being proivded by the parent component App, however as App needs to fetch the data it needs to wait for the video response. The way we have to handle this situation is to add acheck in VideoDetails and dreturnn a simple Loading in VideoDetails if the video is currently being fetched and unvailable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from "./components/SearchBar";
 import YTSearch from 'youtube-api-search';
 import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 
 const API_KEY = 'AIzaSyATS38RuWTxH2vL9QSu_NTjE5JXlU3MzgU';
 
@@ -24,6 +25,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos}/>
             </div>
         );
